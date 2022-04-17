1c6888840f3b3bd305c6bf1e60f462cc57f9004d
Vass Zsolt <rtmzsolt@gmail.com>
2022-04-17 23:04:45 +0200

    A possible way to handle taken fields

--- src/main/java/Game/GameLogic/Board.java
+++ src/main/java/Game/GameLogic/Board.java
@@ -1,7 +1,11 @@
 package Game.GameLogic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
     private ChessPiece [][] board = new ChessPiece[8][8];
+    private List<Position> takenPositions=new ArrayList<>();
 
     public ChessPiece[][] InitialiseNewBoard(){
         board[0][6]=new ChessPiece(
@@ -15,6 +19,20 @@ public class Board {
                 ChessPiece.PieceType.KNIGHT,
                 new Position(7,1)
         ); //B1
+        takenPositions.add(new Position(0,6));
+        takenPositions.add(new Position(7,1));
         return board;
     }
+
+    public void takePosition(Position position){
+        if(!takenPositions.contains(position)){
+            takenPositions.add(position);
+        }
+    }
+
+    public List<Position> getTakenPositions(){
+        return this.takenPositions;
+    }
+
+
 }
